03434284ff8528a67b9010273733d645fedf9afa
pang <phbtttttt@gmail.com>
2019-06-07 15:57:01 +0800

    add rental list component, rental detail component and rental service and rental model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'phb-cli';
+  componentTitle = 'componentTitle';
+  clickHandler() {
+    alert("click me")
+  }
 }
